ad4b61357db1acbaa627bd136ccc0de9a69580fd
AlphaDave <dave@alphasoftware.com>
2016-07-25 11:31:11 -0400

    First Commit

---
+++ ConferenceApp.WebProjects/Default.WebProject/PhoneGapProjects/confapp/www/_phoneGapSettings.js
@@ -0,0 +1,46 @@
+function _phoneGapSettings() { 
+ return {
+    "appId": "com.alphasoftware.devcon2016",
+    "preferences": {
+        "permissions": "none",
+        "orientation": "default",
+        "target-device": "universal",
+        "fullscreen": "false",
+        "webviewbounce": "true",
+        "prerendered-icon": "false",
+        "stay-in-webview": "false",
+        "ios-statusbarstyle": "default",
+        "detect-data-types": "true",
+        "exit-on-suspend": "false",
+        "show-splash-screen-spinner": "true",
+        "auto-hide-splash-screen": "true",
+        "disable-cursor": "false",
+        "android-minSdkVersion": "7",
+        "android-installLocation": "auto",
+        "phonegap-version": "cli-5.2.0",
+        "FadeSplashScreen": "true",
+        "FadeSplashScreenDuration": "2",
+        "EnableViewportScale": "false",
+        "MediaPlaybackRequiresUserAction": "true",
+        "AllowInlineMediaPlayback": "false",
+        "TopActivityIndicator": "gray",
+        "BackupWebStorage": "none",
+        "iosPersistentFileLocation": "Library",
+        "KeyboardDisplayRequiresUserAction": "true",
+        "SurpressesIncrementalRendering": "true",
+        "android-maxSdkVersion": "",
+        "android-targetSdkVersion": "",
+        "KeepRunning": "true",
+        "splash-screen-duration": "5000",
+        "ErrorUrl": "error.html",
+        "LoadingDialog": "Please wait, the app is loading.",
+        "LoadingPageDialog": "Please wait, the data is loading.",
+        "LoadUrlTimeoutValue": "20000",
+        "AndroidPersistentFileLocation": "Internal"
+    },
+    "plugins": {
+        "file": "npm",
+        "file-transfer": "npm"
+    }
+};
+}
